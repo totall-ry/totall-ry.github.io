e2c878f5ba66a28b0bc24ce9a180de5df354b1ce
totall_ry <ryanmok2007@gmail.com>
2026-06-15 16:46:20 +0800

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDo34O7HPvy_pGFG9faPvsR-sFk0ZUwDwc",
-  authDomain: "campus-bulletin-d9573.firebaseapp.com",
-  projectId: "campus-bulletin-d9573",
-  storageBucket: "campus-bulletin-d9573.firebasestorage.app",
-  messagingSenderId: "699840177154",
-  appId: "1:699840177154:web:ee6c31b470f4c9cb6cf31b",
-  measurementId: "G-ZZ25R5GD8P"
+    apiKey: "AIzaSyDo3407HPVy_pGFG9faPvsR-sFk0ZUwDwc",
+    authDomain: "campus-bulletin-d9573.firebaseapp.com",
+    projectId: "campus-bulletin-d9573",
+    storageBucket: "campus-bulletin-d9573.firebasestorage.app",
+    messagingSenderId: "69984017154",
+    appId: "1:69984017154:web:ee6c31b470f4c9cb6cf31b",
+    measurementId: "G-ZZ25R5GD8P"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+window.db = getFirestore(app);
